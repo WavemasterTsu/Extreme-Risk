92937d4cd0ecbb57e87646d204bce415a2e8f60a
Eric Hill <Wavemaster14@aol.com>
2012-05-23 21:35:11 -0400

    Added simple menu system. Compiles.

--- ExtremeRiskProject/ExtremeRisk/Views/BaseView.py
+++ ExtremeRiskProject/ExtremeRisk/Views/BaseView.py
@@ -1,11 +1,25 @@
 import Tkinter
 
 class BaseView(Tkinter.Tk):   
-     
+             
+    def CreateMenu(self):
+        objMenu = Tkinter.Menu(self)
+        
+        objFileMenu = Tkinter.Menu(objMenu, tearoff=0)
+        objFileMenu.add_command(label="New")
+        objFileMenu.add_command(label="Load")
+        objFileMenu.add_command(label="Save")
+        objFileMenu.add_command(label="Exit")
+        
+        objMenu.add_cascade(label="File", menu=objFileMenu)
+        
+        return objMenu
+    
     def __init__(self, parent):
         Tkinter.Tk.__init__(self, parent)
         self.parent = parent
         self.initialize()
-        
+    
     def initialize(self):
-        pass
+        objMenu = self.CreateMenu()
+        self.config(menu=objMenu)
--- ExtremeRiskProject/ExtremeRisk/Views/IntroView.py
+++ ExtremeRiskProject/ExtremeRisk/Views/IntroView.py
@@ -3,9 +3,10 @@ from BaseView import BaseView
 class IntroView(BaseView):
     
     def __init__(self, parent):
-        BaseView.__init__(self, parent)
+        BaseView.__init__(BaseView(self), parent)
         self.parent = parent
         self.initialize()
         
     def initialize(self):
+        print "Hello World"
         pass
